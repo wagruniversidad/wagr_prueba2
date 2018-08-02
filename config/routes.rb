6a4b3dec7a526df2bf4763002eefedab80a71493
LAURARUIZ <nellyhernandez@gmail.com>
2018-08-02 16:15:15 -0500

    initrails

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  
+  resource :posts
+  root 'welcome#index'
+ 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ 
+
+end
